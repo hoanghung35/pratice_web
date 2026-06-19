22619933f78b4dad202ff6c339494b096bf226a7
_Felix <104049480+hoanghung35@users.noreply.github.com>
2026-06-19 14:20:26 +0700

    Change Params from ApproveDto

--- Content_App/App/DTOs/Approve/ApproveDto.cs
+++ Content_App/App/DTOs/Approve/ApproveDto.cs
@@ -2,5 +2,17 @@
 {
     public class ApproveDto
     {
+        public Guid id {get; set;}
+        public string picName {get; set;} = null!;
+        public string dept {get; set;} = null!;
+        public string itemName {get; set;} = null!;
+        public int qty {get; set;}
+        public string kind {get; set;} = null!;
+        public string requestorCode {get; set;} = null!;
+        public string requestorName {get; set;} = null!;
+        public string purpose {get; set;} = null!;
+        public string datRequest {get; set;} = null!;
+        public string? planRequest {get; set;}
+        public string status {get; set;} = null!;
     }
 }
