184385f9338dc5f068fff0f6425f061f0302a15a
_Felix <104049480+hoanghung35@users.noreply.github.com>
2026-06-19 13:56:29 +0700

    Update Item Controller

--- Content_App/Controllers/ItemController.cs
+++ Content_App/Controllers/ItemController.cs
@@ -9,69 +9,118 @@ namespace Content_App.Controllers
 {
     [Route("api/items")]
     [ApiController]
+    [IgnoreAntiforgeryToken]
     public class ItemController : Controller
     {
         private readonly ItemService _itemService;
         private readonly ActionService _actionService;
+        private readonly FileService _fileService;
+        private readonly RequestService _requestService;
+        private readonly ApproveService _approveService;
 
-        public ItemController(ItemService itemService, ActionService actionService)
+        public ItemController(ItemService itemService, 
+            ActionService actionService, 
+            FileService fileService, 
+            RequestService requestService, 
+            ApproveService approceService
+            )
         {
             this._itemService = itemService;
             this._actionService = actionService;
+            this._fileService = fileService;
+            this._requestService = requestService;
+            this._approveService = approveService;
         }
 
-        [Authorize(Policy ="dev")]
-        [HttpGet]
-        public async Task<IActionResult> GetAllItem()
-        {
-            return Ok(await _itemService.GetAllAsync());
-        }
+       [Authorize]
+       [HttpGet]
+       public async Task<IActionResult> GetItem() {
+           var roleName = User.FindFirst(JwtClaimConstant.Role)?.Value!;
+           var userId = User.FindFirst(JwtClaimConstant.UserId)?.Value!;
 
-        [Authorize(Policy ="admin")]
-        [HttpGet("limit")]
-        public async Task<IActionResult> GetItemByLocation(Guid deptId, Guid areaId)
-        {
-            return Ok(await _itemService.GetItemByLocationAsync(deptId, areaId));
-        }
+           return Ok(await _itemService.GetItemAsync(roleName, new Guid(userId)));
+       }
 
-        [Authorize(Policy = "CanCreate")]
-        [HttpPost("create")]
-        public async Task<IActionResult> Create(ItemDto dto)
-        {
-            await _itemService.CreateAsync(dto);
-            await _actionService.NewItemActionAsync(dto);
+       [Authorize]
+       [HttpPost("create-item")]
+       public async Task<IActionResult> CreateItem([FromBody] ItemDto dto) {
+           var userId = User.FindFirst(JwtClaimConstant.UserId)?.Value!;
+           var roleName = User.FindFirst(JwtClaimConstant.Role)?.Value!;
+           var picId = User.FindFirst(JwtClaimConstant.PicId)?.Value!;
+           var newItemId = await _itemService.CreateItemAsync(newItem, new Guid(userId));
+           await _actionService.NewItemActionAsync(newItem, new Guid(userId), new Guid(picId), newItemId);
 
-            return Ok();
-        }
+           return Ok();
+       }
 
-        [Authorize(Policy ="dev")]
-        [HttpPut("update")]
-        public async Task<IActionResult> Update(Item item)
-        {
-            await _itemService.UpdateItemAsync(item);
+       [Authorize]
+       [HttpPut("update-item")]
+       public async Task<IActionResult> UpdateItem(ItemDto item) {
+           await _itemService.UpdateItemAsync(item);
 
-            return Ok();
-        }
+           return Ok();
+       }
 
-        [Authorize(Policy ="dev")]
-        [HttpDelete("delete/{id}")]
-        public async Task<IActionResult> DeleteItem(string id)
-        {
-            await _itemService.DeleteAsync(id);
+       [Authorize]
+       [HttpPost("change-image/{itemId}")]
+       public async Task<IActionResult> ItemUpdateImage(IFormFile file, Guid itemId) {
+           await _itemService.ItemUpdateImageAsync(file, itemId);
 
-            return Ok();
-        }
+           return Ok();
+       }
 
-        [HttpGet("export-item")]
-        public IActionResult Export()
-        {
-            using (var package = new ExcelPackage())
-            {
-                var worksheet = package.Workbook.Worksheets.Add("Sheet1");
-                // Đổ dữ liệu vào...
-                var content = package.GetAsByteArray();
-                return File(content, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "Report.xlsx");
-            }
-        }
+       [Authorize]
+       [HttpPut("delete-item/{id}")]
+       public async Task<IActionResult> DeleteItem(Guid itemId) {
+           var picId = User.FindFirst(JwtClaimConstant.PicId)?.Value!;
+           await _itemService.DeleteItemAsync(itemId, new Guid(picId));
+
+           return Ok();
+       }
+
+       [Authorize]
+       [HttpGet("export-item")]
+       public async Task<IActionResult> ExportFileItem() {
+           var userId = User.FindFirst(JwtClaimConstant.UserId)?.Value!;
+           var role = User.FindFirst(JwtClaimConstant.Role)?.Value!;
+           var fileName = DateTime.UtcNow.AddHours(7).ToString("dd-MM-yyyy") + "item.xlsx";
+           var data = await _fileService.ExportFileItem(new Guid(userId), role);
+
+           return File(data, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", fileName);
+       }
+
+       [Authorize]
+       [HttpPut("receive-item")]
+       public async Task<IActionResult> ReceiveItem([FromBody] ItemActivityDto dto) {
+           var userId = new Guid(User.FindFirst(JwtClaimConstant.UserId)?.Value!);
+           var picId = new Guid(User.FindFirst(JwtClaimConstant.PicId)?.Value!);
+           await _actionService.ReceiveItemAsync(dto, userId, picid);
+
+           return Ok();
+       }
+
+       [Authorize]
+       [HttpPut("delivery-item")]
+       public async Task<IActionResult> DeliveryItem([FromBody] ItemActivityDto dto) {
+           var userId = new Guid(User.FindFirst(JwtClaimConstant.UserId)?.Value!);
+           var picId = new Guid(User.FindFirst(JwtClaimConstant.PicId)?.Value!);
+
+           var res = await _actionService.DeliveryItemAsync(dto, userId, picId);
+           var reqId = await _requestService.CreateRequestAsync(res, picId);
+           await _approveService.CreateApproveAsync(null, reqId);
+
+           return Ok();
+       }
+
+       [Authorize]
+       [HttpPost("create-list-item")]
+       public async Task<IActionResult> CreateListItemFromFile(IFormFile file) {
+           var userId = new Guid(User.FindFirst(JwtClaimConstant.UserId)?.Value!);
+           var picId = new Guid(User.FindFirst(JwtClaimConstant.PicId)?.Value!);
+
+           await _itemService.CreateListItemFromFileAsync(file, userId, picId);
+
+           return Ok();
+       }
     }
 }
