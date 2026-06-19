1ee03dc01623b4a722c0f644b9558993c991d17c
_Felix <104049480+hoanghung35@users.noreply.github.com>
2026-06-19 14:25:41 +0700

    Change params for History Dto

--- Content_App/App/DTOs/History/HistoryDto.cs
+++ Content_App/App/DTOs/History/HistoryDto.cs
@@ -1,1 +1,13 @@
-
+namespace Content_App.App.DTOs.History
+{
+    public class History 
+    {
+        public string empCode {get; set;} = null!;
+        public string itemName {get; set;} = null!;
+        public string pic {get; set;} = null!;
+        public int qty {get; set;} = 0;
+        public string kind {get; set;} = null!;
+        public string reason {get; set;} = null!;
+        public string dateAction {get; get;} = null!;
+    }
+}
