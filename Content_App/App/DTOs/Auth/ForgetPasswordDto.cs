ba5a87e57c4e1bc1de05b9a9278c5145538bff27
_Felix <104049480+hoanghung35@users.noreply.github.com>
2026-06-19 14:21:57 +0700

    Change params for forgetPw func

--- Content_App/App/DTOs/Auth/ForgetPasswordDto.cs
+++ Content_App/App/DTOs/Auth/ForgetPasswordDto.cs
@@ -1,6 +1,6 @@
 ﻿namespace Content_App.App.DTOs.Auth
 {
-    public class ResetPasswordDto
+    public class ForgetPasswordDto
     {
         public string Email { get; set; } = null!;
         public string UserCode { get; set; } = null!;
