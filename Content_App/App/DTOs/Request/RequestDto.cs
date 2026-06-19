8d1f28d7a21bbb2c70ab034713bf1f7ac1598d92
_Felix <104049480+hoanghung35@users.noreply.github.com>
2026-06-19 15:31:14 +0700

    add params
    Created RequestDto class with properties for item details and request information.

---
+++ Content_App/App/DTOs/Request/RequestDto.cs
@@ -0,0 +1,17 @@
+﻿namespace Content_App.App.DTOs.Order
+{
+    public class RequestDto
+    {
+        public Guid id { get; set; }
+        public string itemName { get; set; } = null!;
+        public string itemName { get; set; } = null!;
+        public int qty { get; set; }
+        public string kind { get; set; } = null!;
+        public string requestorName { get; set; } = null!;
+        public string requestorCode { get; set; } = null!;
+        public string? reason { get; set; }
+        public string dateRequest { get; set; } = null!;
+        public string planDate { get; set; } = null!;
+        public string status { get; set; } = null!;
+    }
+}
