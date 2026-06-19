487e73bb3ae4e0717035c6e758f23ad4702b9556
_Felix <104049480+hoanghung35@users.noreply.github.com>
2026-06-19 14:09:04 +0700

    Update RequestController

---
+++ Content_App/Controllers/RequestController.cs
@@ -0,0 +1,26 @@
+﻿using Content_App.App.Services;
+using Microsoft.AspNetCore.Mvc;
+using Content_App.Shared.Constants;
+using Microsoft.AspNetCore.Authorization;
+
+namespace Content_App.Controllers
+{
+    [Route("api/request")]
+    [ApiController]
+    public class RequestController : ControllerBase
+    {
+        private readonly RequestService _requestService;
+
+       public RequestController(RequestService requestService) {
+           this._requestService = requestService;
+       }
+
+       [Authorize]
+       [HttpGet]
+       public async Task<IActionResult> GetRequest() {
+           var picId = new Guid(User.FindFirst(JwtClaimConstant.PicId)?.Value!);
+
+           return Ok(await _requestService.GetRequestAsync(picId));
+       }
+    }
+}
