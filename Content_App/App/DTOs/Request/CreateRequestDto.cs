a7a206c9e1ac856e6c86e6227d252537ec4c2d1b
_Felix <104049480+hoanghung35@users.noreply.github.com>
2026-06-19 15:25:38 +0700

    add params for item dto

---
+++ Content_App/App/DTOs/Request/CreateRequestDto.cs
@@ -0,0 +1,12 @@
+﻿namespace Content_App.App.DTOs.Order
+{
+    public class CreateRequestDto
+    {
+        public Guid itemId { get; set; }
+        public int qty { get; set; }
+        public DateTime? planRequest { get; set; }
+        public string? Reason { get; set; }
+        public string? Ucode { get; set; }
+        public string? Uname { get; set; }
+    }
+}
