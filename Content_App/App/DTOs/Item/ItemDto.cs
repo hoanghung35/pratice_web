78c7f09fdf58ee480e3514f6b79b4f33688e0fcb
_Felix <104049480+hoanghung35@users.noreply.github.com>
2026-06-19 14:57:08 +0700

    update params

--- Content_App/App/DTOs/Item/ItemDto.cs
+++ Content_App/App/DTOs/Item/ItemDto.cs
@@ -6,5 +6,15 @@
         public Guid DeptId { get; set; }
         public string ItemCode { get; set; ] = null!;
         public string EnName { get; set; } = null!;
+        public string? DeptName { get; set; }
+        public string VnName { get; set; } = null!;
+        public string Maker { get; set; } = null!;
+        public string Supplier { get; set; } = null!;
+        public string PositionIn { get; set; } = null!;
+        public int Quantiry { get; set; }
+        public string Unit { get; set; } = null!;
+        public string Currency { get; set; } = null!;
+        public decimal? Cost { get; set; }
+        public string? Image { get; set; }
     }
 }
