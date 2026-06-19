a63e517a74f7d1e55d55373be20ab28fc84676a5
_Felix <104049480+hoanghung35@users.noreply.github.com>
2026-06-19 15:43:45 +0700

    Create & add params fro CurrencyDto

---
+++ Content_App/App/DTOs/User/CurrencyDto.cs
@@ -0,0 +1,8 @@
+namespace Content_App.App.DTOs.User
+{
+    public class CurrencyDto
+    {
+        public string currenName { get; set; } = null!;
+        public decimal currenValue { get; set; } = 0;
+    }
+}
