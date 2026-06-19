599d435e4f422681abb6c112ff3c1ef2e381230a
_Felix <104049480+hoanghung35@users.noreply.github.com>
2026-06-19 15:38:57 +0700

    Create & add params AccountInitDto

---
+++ Content_App/App/DTOs/User/AccountInitDto.cs
@@ -0,0 +1,12 @@
+namespace Content_App.App.DTOs.User
+{
+    public class AccountInitDto
+    {
+      public string fullName { get; set; } = null!;
+      public string userCode { get; set; } = null!;
+      public string email { get; set; } = null!;
+      public string dept { get; set; } = null!;
+      public string area { get; set; } = null!;
+      public string role { get; set; } = null!;
+    }
+}
