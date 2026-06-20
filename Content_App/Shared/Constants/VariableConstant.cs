6d172fa73fcf42222c567278dcfe26594a3bc458
_Felix <104049480+hoanghung35@users.noreply.github.com>
2026-06-20 16:54:01 +0700

    Add new constants for image path and website

--- Content_App/Shared/Constants/VariableConstant.cs
+++ Content_App/Shared/Constants/VariableConstant.cs
@@ -6,5 +6,9 @@
         public const string HostName = "LIC System";
         public const string HostAddress = "lics@local.canon-vn.com.vn";
         public const string AllowedChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789_";
+        public const string pathImage = "";
+        public const int totalDayKeep = 5;
+        public const string pathWebsite = "http://10.16.3.167:2024/";
+        public const string pathRecord = ".txt";
     }
 }
