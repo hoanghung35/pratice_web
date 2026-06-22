58becc04f3397171e643f5c65aa8cc1ebac7b8aa
_Felix <104049480+hoanghung35@users.noreply.github.com>
2026-06-22 11:32:16 +0700

    add functions to approve service

--- src/app/core/services/approve.service.ts
+++ src/app/core/services/approve.service.ts
@@ -1,9 +1,31 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Approve } from '../shared/models/approve.model';
+import { apiUrl } from '../constant/app.constant';
+import { UUID } from 'crypto';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApproveService {
+  url = `${apiUrl}/approve-request`;
+  
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
+  getApprove(){
+    return this.http.get<Approve[]>(this.url);
+  }
+
+  approve(id: UUID) {
+    return this.http.post<any>(`${this.url}/approve/${id}`, { withCredentials: true });
+  }
+
+  reject(id: UUID) {
+    return this.http.post<any>(`${this.url}`/reject/${id});
+  }
+
+  approveAll(data: any[]): Observable<any> {
+    return this.http.put<any>(`${this.url}/approve/all`, data, { withCredentials: true });
+  }
 }
