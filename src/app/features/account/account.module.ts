1e9a98160d103dd78d7914edda42c2fd7b130a10
_Felix <104049480+hoanghung35@users.noreply.github.com>
2026-06-22 15:20:05 +0700

    account module update

--- src/app/features/account/account.module.ts
+++ src/app/features/account/account.module.ts
@@ -1,16 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-import { UsersComponent } from './users.component';
+import { AccountComponent } from './account.component';
 import { SharedModule } from '../../shared/shared.module';
 
 @NgModule({
     imports: [
         CommonModule,
-        UsersComponent,
+        AccountComponent,
         RouterModule.forChild([
-            { path: '', component: UsersComponent }
+            { path: '', component: AccountComponent }
         ])
     ]
 })
-export class UsersModule { }
+export class AccountModule { }
