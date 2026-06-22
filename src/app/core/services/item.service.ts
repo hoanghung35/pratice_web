55ae2e40a5c2b36258559c4527cbfd1a64c792e8
_Felix <104049480+hoanghung35@users.noreply.github.com>
2026-06-22 13:46:21 +0700

    update item service

--- src/app/core/services/item.service.ts
+++ src/app/core/services/item.service.ts
@@ -8,43 +8,72 @@ import { blob } from 'stream/consumers';
   providedIn: 'root'
 })
 export class ItemService {
-
+  url = `${apiUrl}/items`;
 
   constructor(private http: HttpClient) { }
 
-  getItems() {
-    return this.http.get<Item[]>('http://localhost:5251/api/items');
+  getItems(): Observable<Item[]> {
+    return this.http.get<Item[]>(this.url);
   }
 
-  getItemsWithCondition(fromD: Date, toD: Date) {
-    const params = new HttpParams()
-      .set('fromDate', fromD.toISOString())
-      .set('toDate', toD.toISOString());
-
-    this.http.get('http://localhost:5251/api/items', { params });
+  createItem(item: Item) {
+    return this.http.post<Item>(`${this.url}/create-item`, item, { withCredentials: true });
   }
 
-  createItem(item: Partial<Item>) {
-    return this.http.post<Item>('http://localhost:5251/api/items', item);
+  search(items: item[], text: string) {
+    if(!text) return [];
+
+    return items.filter(item => 
+        item.itemCode.toLowerCase.includes(text.toLowerCase().trim()) ||
+        item.enName.toLowerCase.includes(text.toLowerCase().trim()) ||
+        item.vnName.toLowerCase.includes(text.toLowerCase().trim()) ||
+        item.maker.toLowerCase.includes(text.toLowerCase().trim()) ||
+        item.positionIn.toLowerCase().includes(text.toLowerCase().trim()) ||
+        item.deptName.toLowerCase().includes(text.toLowerCase().trim())
+    );
   }
 
-  updateItem(id: string, item: Partial<Item>) {
-    return this.http.put<Item>(`http://localhost:5251/api/items/${id}`, item);
+  update(data: Item) {
+    return this.http.put<Item>(`${this.url}/update-item`, data, { withCredentials: true });
   }
 
-  importFile(file: File): Observable<HttpEvent<any>> {
+  deleteItem(id: UUID) {
+    return this.http.put<any>(`${this.url}/delete-item/${id}`, { withCredentials: true });
+  }
+
+  updateImage(file: any, id: UUID) {
+    return this.http.post<any>(`${this.url}/change-image/${id}`, file, { withCredentials: true });
+  }
+
+  exportFile() {
+    return this.http.get(`${this.url}/export-item`, { responseType: 'blob' });
+  }
+
+  createListItem(file: File) {
     const formData = new FormData();
-    formData.append('file', file, file.name);
+    formData.append('file', file, file.name)
+    ;
 
-    return this.http.post('http://localhost:5251/api/items/import-file', formData, {
-      reportProgress: true,
-      observe: 'events'
-    });
+    return this.http.post(`${this.url}/create-list-item`, formData,
+      {
+        reportProgress: true,
+        observe: 'events'
+      });
   }
 
-  exportItem() {
-    return this.http.get('http://localhost:5251/api/export-item', { responseType: 'blob' }).subscribe(blob => {
-      //saveAs(blob, 'report.xlsx');
-    })
+  receiveItem(item: Item, qty: number) {
+    let data = {
+      itemId: item.id,
+      empCode: null,
+      empName: null,
+      qty: qty,
+      reason: null
+    };
+
+    return this.http.put<any>(`${this.url}/receive-item`, data, { withCredentials: true });
+  }
+
+  deliveryItem(data: any) {
+    return this.http.put<any>(`${this.url}/delivery-item`, data, { withCredentials: true })
   }
 }
