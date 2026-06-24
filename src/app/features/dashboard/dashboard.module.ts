e340f39517212398d06dd20313c1336e73748ddc
_Felix <104049480+hoanghung35@users.noreply.github.com>
2026-06-24 16:33:28 +0700

    upd dashboard module

--- src/app/features/dashboard/dashboard.module.ts
+++ src/app/features/dashboard/dashboard.module.ts
@@ -1,1 +1,14 @@
+import { NgModule } form '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { DashboardComponent } from './dashboard.component';
 
+@NgModule({
+  imports: [
+    CommonModule,
+    RouterModule.forChild([
+      {path: '', component: DashboardComponent}
+    ])
+  ]
+})
+export class DashboardModule { }
