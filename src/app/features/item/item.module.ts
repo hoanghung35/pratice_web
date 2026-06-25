8bc89297dca4bab46f1929cbaeffff954cd1157d
_Felix <104049480+hoanghung35@users.noreply.github.com>
2026-06-25 17:23:34 +0700

    udp item module

--- src/app/features/item/item.module.ts
+++ src/app/features/item/item.module.ts
@@ -2,20 +2,10 @@ import { NgModule } from "@angular/core";
 import { ItemComponent } from "./item.component";
 import { CommonModule } from "@angular/common";
 import { RouterModule } from "@angular/router";
-import { SharedModule } from "../../shared/shared.module";
-import { MatDialogModule } from '@angular/material/dialog';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { CreateItemComponent } from './item.dialog/create.item/create.item.component';
 
 @NgModule({
-    declarations: [ItemComponent, CreateItemComponent],
     imports: [
         CommonModule,
-        SharedModule,
-        MatDialogModule,
-        MatFormFieldModule,
-        MatInputModule,
         RouterModule.forChild([
             { path: '', component: ItemComponent }
         ])
