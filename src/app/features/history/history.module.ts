c935d2754dedf3c85e16c75548d8a694090ec14d
_Felix <104049480+hoanghung35@users.noreply.github.com>
2026-06-25 11:52:57 +0700

    Initial history module

--- src/app/features/history/history.module.ts
+++ src/app/features/history/history.module.ts
@@ -1,1 +1,15 @@
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { HistoryComponent } from './history.component';
+import { NgModule } from '@angular/core';
 
+@NgModule({
+  imports: [
+    CommonModule,
+    RouterModule.forChild([
+      { paht: '', component: HistoryComponent }
+    ])
+  ]
+})
+
+export class HistoryModule { }
