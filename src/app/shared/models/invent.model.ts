8677d420286457c67d72fc8585bedb84c604223f
_Felix <104049480+hoanghung35@users.noreply.github.com>
2026-06-26 16:34:45 +0700

    Add Invent interface for inventory management

---
+++ src/app/shared/models/invent.model.ts
@@ -0,0 +1,11 @@
+export interface Invent {
+    itemNo: string;
+    itemName: string;
+    unit: string;
+    price: number;
+    stock: number;
+    input: number;
+    output: number;
+    actualStock: string;
+    totalAmount: string;
+}
