4dd3227a4194fd2a8bf534b24f571f84607780b6
_Felix <104049480+hoanghung35@users.noreply.github.com>
2026-06-26 15:23:20 +0700

    udp upload file item

--- src/app/features/item/item.dialog/upload-file-item/upload-file-item.component.ts
+++ src/app/features/item/item.dialog/upload-file-item/upload-file-item.component.ts
@@ -1,1 +1,49 @@
 
+
+
+
+
+
+@Component({
+  selector: 'app-upload-file-item',
+  templateUrl: './upload-file-item.component.html',
+  styleUrl: './upload-file-item.component.scss',
+  standalone: true,
+  imports: [SharedModule]
+})
+export class UploadFileItemComponent {
+  file!: File;
+
+  constructor(
+    private itemService: ItemService,
+    private dialogRef: MatDialogRef<UploadFileItemComponent>,
+    private dialogService: DialogService
+  ){}
+
+  onFileChange(event: any) {
+    this.file = event.target.files[0];
+  }
+
+  submit() {
+    if(this.file) {
+      this.itemService.createLlistItem(this.file).pipe(
+        filter(event => event.type === HttpEventType.Response)
+      ).subscribe({
+        next: () => {
+          this.dialogRef.close();
+
+          this.dialogService.AutoAlterDialog({
+            message: 'Create Item Success!',
+            showcheck: true
+          });
+        },
+        error: (err) => {
+          this.dialogService.AutoAlterDialog({
+            message: 'Create Item Failed!',
+            showcheck: false
+          });
+        }
+      });
+    }
+  }
+}
