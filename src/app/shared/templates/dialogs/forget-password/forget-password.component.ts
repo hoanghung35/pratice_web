0ac0a4906e2911ca21e5d8c7876d4f9ccf6a4e90
_Felix <104049480+hoanghung35@users.noreply.github.com>
2026-06-30 16:10:30 +0700

    udp forget-pw component

--- src/app/shared/templates/dialogs/forget-password/forget-password.component.ts
+++ src/app/shared/templates/dialogs/forget-password/forget-password.component.ts
@@ -1,1 +1,44 @@
+import { Componentm, Inject } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 
+@Component({
+  selector: 'app-forget-password',
+  templateUrl: './forget-password.component.html',
+  styleUrl: './forget-password.component.scss'
+})
+export class ForgetPasswordComponent {
+  forgetcode: string = '';
+  forgetemail: string = '';
+  requiredcode: boolean = false;
+  requiredemail: boolean = false;
+  forgetData!: FormGroup;
+
+  constructor(
+    private authService: AuthSerivec,
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    private dialogRef: MatDialogRef<ForgetPasswordComponent>
+  ){}
+
+  ngOnInit() {
+    this.forgetData = new FormGroup({
+      forgetcode: new FormControl(''),
+      forgetemail: new FormControl('')
+    });
+
+    this.requiredcode = false;
+    this.requiredemail = false;
+  }
+
+  resetPassword(data: any) {
+    this.forgetcode = data.forgetcode;
+    this.forgetemail = data.forgetemail;
+
+    if(this.forgetcode == '' || this.forgetemail == '') {
+      this.requiredcode = true;
+    }
+    else {
+      this.dialogRef.close(data = {usercode: this.forgetcode, email: this.forgetemail});
+    }
+  }
+}
