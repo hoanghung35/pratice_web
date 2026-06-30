ee46524702fb1eefad56c76abf98cf356ac1c695
_Felix <104049480+hoanghung35@users.noreply.github.com>
2026-06-30 16:52:47 +0700

    udp shared module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,33 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ReactiveFormsModule } from '@angular/forms';
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
-import { MatDialogModule } from '@angular/material/dialog';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { TableComponent } from './table/table.component';
-import { DialogComponent } from './dialog/dialog.component';
+import { MaterialModule } from './material-module'
+
 
 @NgModule({
     imports: [
         CommonModule,
-        ReactiveFormsModule,
-        MatButtonModule,
-        MatIconModule,
-        MatDialogModule,
-        MatFormFieldModule,
-        MatInputModule,
-        TableComponent
+        TableComponent,
     ],
     exports: [
         CommonModule,
-        MatButtonModule,
-        MatIconModule,
         TableComponent,
-        // LoadingComponent,
-        // RolePipe,
-        // DatePipe
+        MaterialModule
     ],
     declarations: [
         DialogComponent
