355d09a5cda34b2ff9994dcd93c358ab5e59b9ed
_Felix <104049480+hoanghung35@users.noreply.github.com>
2026-06-30 14:06:23 +0700

    udp page-error component

--- src/app/shared/templates/page-error/page-error.component.ts
+++ src/app/shared/templates/page-error/page-error.component.ts
@@ -1,1 +1,15 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
+@Component({
+  selector: 'app-page-error',
+  templateUrl: './page-error.component.html',
+  styleUrl: './page-error.component.scss'
+})
+export class PageErrorComponent {
+  constructor(private router: Router) {}
+
+  GoToHome() {
+    this.router.navigate(['/dashboard']);
+  }
+}
