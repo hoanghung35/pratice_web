27870af6a71358d49306cc744ec63347aa553228
_Felix <104049480+hoanghung35@users.noreply.github.com>
2026-06-30 15:47:11 +0700

    Add AlterComponent for dialog handling

--- src/app/shared/templates/dialogs/alter/alter.component.ts
+++ src/app/shared/templates/dialogs/alter/alter.component.ts
@@ -1,1 +1,32 @@
+import { Component, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 
+export interface DialogData {
+  message: string;
+  showcheck: boolean;
+}
+
+@Component({
+  selector: 'app-alter',
+  templateUrl: './alter.component.html',
+  styleUrl: './alter.component.scss',
+})
+export class AlterComponent implements OnInit {
+  success: boolean = false;
+  failed: boolean = false;
+  alterData!: DialogData;
+
+  constructor(MAT_DIALOG_DATA public data: DialogData) {}
+
+  ngOnInit() {
+    this.alterData = this.data;
+
+    if(this.alterData.showcheck) {
+      this.success = true;
+      this.failed = false;
+    } else {
+      this.failed = true;
+      this.success = false;
+    }
+  }
+}
